acc438cf28f37fa1bd405f177dfd18809457b474
ian kopke <iankopke2005@Gmail.com>
2025-03-04 20:30:44 -0500

    working on the form for Message.tsx

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -1,17 +1,36 @@
-
+import { Form, FormGroup, Input, Label } from "reactstrap";
+import "../css/message.css"
 
 
 const Message = () => {
 
     return (
+
         <div>
+        
           <h1 className="display-3">Hello World!</h1>
           <p className="lead">
             Welcome to Submit a Message! <br></br>
             Check out <code>src/pages/Home.tsx</code> to see how you can get
             started.
           </p>
-          
+        
+            <div className="messageForm">
+                <Form>
+                    <FormGroup>
+                    <Label for="messageBody">
+                    Message
+                    </Label>
+                    <Input
+                    id="messageBody"
+                    name="body"
+                    placeholder="Type your message here!"
+                    className="messageBody"
+                    />
+                    </FormGroup>
+                </Form>
+
+            </div>
         </div>
       );
 
