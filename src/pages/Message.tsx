e6bb60aecf51dfb3eebb0bc335e5cbfb86b3c87b
ian kopke <iankopke2005@Gmail.com>
2025-03-07 00:19:43 -0500

    working on the submit form

--- src/pages/Message.tsx
+++ src/pages/Message.tsx
@@ -1,9 +1,11 @@
-import { Form, FormGroup, Input, Label } from "reactstrap";
+import { Form, FormGroup, Input, Label, Button, ButtonGroup } from "reactstrap";
 import "../css/message.css"
+import { useState } from "react";
 
 
 const Message = () => {
-
+    const [rSelected, setRSelected] = useState(null);
+    const animation: string[] = ["Default CSH", "Just Scrolling", "L Bozo"]
     return (
 
         <div>
@@ -28,6 +30,43 @@ const Message = () => {
                     className="messageBody"
                     />
                     </FormGroup>
+                    <h3>What Kind of effects would you like? (More will be added soon @stove frequently so I make more of them)<br></br></h3>
+                    <FormGroup>
+                    <h5>Radio Buttons</h5>
+                        <ButtonGroup>
+                            <Button
+                            color="primary"
+                            outline
+                            onClick={() => setRSelected(animation[0])}
+                            active={rSelected === 1}
+                            >
+                            {animation[0]}
+                            </Button>
+                            <Button
+                            color="primary"
+                            outline
+                            onClick={() => setRSelected(animation[1])}
+                            active={rSelected === 2}
+                            >
+                            {animation[1]}
+                            </Button>
+                            <Button
+                            color="primary"
+                            outline
+                            onClick={() => setRSelected(animation[2])}
+                            active={rSelected === 3}
+                            >
+                            {animation[2]}
+                            </Button>
+                        </ButtonGroup>
+                        {/* <p>Selected: {rSelected}</p> */}
+                        <h3>How long do you want your message to play? -- (ask 3da about drink creds for InfoSys)</h3>
+                        <input className="col-6" type="number" id="replyNumber" min="5" max="45" step="5" data-bind="value:replyNumber" placeholder="in Minutes"/>
+
+                    </FormGroup>
+                    <button className="btn btn-primary btn-hover" >
+                    Submit
+                    </button>{' '}
                 </Form>
 
             </div>
